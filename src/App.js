4c9932e66a3db8685ed9114ba196707a9d80a6a0
Flavia Bulad <flavia.bulad@gmail.com>
2022-06-29 17:57:54 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './Components/Login/LoginPage';
+import SignUpPage from './Components/SignUp/SignUpPage';
+import BalancePage from './Components/Balance/BalancePage';
+import UserContext from './contexts/UserContext';
+import './styles/reset.css';
+import './styles/index.css';
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={{token, setToken}}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/sign-up/" element={<SignUpPage />} />
+        <Route path="/balance/" element={<BalancePage />} />
+      </Routes>
+    </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
