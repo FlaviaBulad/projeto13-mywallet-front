4fd822b076844abe2e38c63d26b8ede631351657
Flavia Bulad <flavia.bulad@gmail.com>
2022-07-02 14:45:27 -0300

    feat: sign-up page

--- src/Components/SignUp/SignUpPage.js
+++ src/Components/SignUp/SignUpPage.js
@@ -1,3 +1,153 @@
-export default function SignUpPage(){
-    
+import styled from 'styled-components';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Spinner from './libs/spinner/Spinner.js';
+
+export default function SignUpPage() {  //Main function,reders the sigh up page
+
+    const signUpDataObject = { //creating sign up data oject 
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+    };
+
+    const navigate = useNavigate();
+    const [signUpData, setSignUpData] = useState(signUpDataObject); //sign up data from forms
+    const [isLoading, setIsLoading] = useState(false);
+
+    function OnChange(e) { // forms OnChange function
+        setSignUpData({ ...signUpData, [e.target.name]: e.target.value }); //filling the sign up object with the forms data
+    }
+
+    function SignUpDataToAPI(e) { //sending all the data to api and redirectioning user to login page if successfull
+        e.preventDefault();
+        setIsLoading(true);
+        const promise = axios.post("https://localhost:5000/sign-up", { ...signUpData });
+        promise.then(() => {
+            setIsLoading(false);
+            navigate("/");
+        });
+        promise.catch((err) => {
+            setIsLoading(false);
+            const errMessage = err.response.statusText;
+            alert(`Post sign-up error: ${errMessage}`)
+        });
+    }
+
+    function BuildingSignUpForms() {
+        return (
+            <>
+
+                <Form onSubmit={SignUpDataToAPI}>
+
+                <Input
+                        type="text" placeholder="nome" name="name"
+                        onChange={OnChange} value={signUpData.name} required
+                    />
+                    <Input
+                        type="email" placeholder="email" name="email"
+                        onChange={OnChange} value={signUpData.email} required
+                    />
+
+                    <Input
+                        type="password" placeholder="senha" name="password"
+                        onChange={OnChange} value={signUpData.password} required
+                    />
+                       <Input
+                        type="password" placeholder="Confirme a senha" name="password_confirmation"
+                        onChange={OnChange} value={signUpData.password_confirmation} required
+                    />
+                    
+                    <Button type="submit" disabled={isLoading}>
+                                {isLoading
+                                ? <Spinner type="ThreeDots" color="#FFFFFF" height={50} width={50} />
+                                : "Cadastrar"}
+                    </Button>
+                </Form>
+
+                <StyledLink to="/">
+                    Já tem uma conta? Entre agora!
+                </StyledLink>
+            </>
+        );
+    }
+
+    const signUpForms = BuildingSignUpForms();
+
+    return (
+        <Container>
+            {signUpForms}
+        </Container>
+    );
 }
+
+//style
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  img{
+    margin-top: 80px;
+  }
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 30px 0 25px;
+`;
+
+const Input = styled.input`
+  width: 300px;
+  height: 45px;
+  margin-bottom: 6px;
+  padding: 10px;
+  border: 1px solid #D5D5D5;
+  border-radius: 5px;
+  background-color:"#FFFFFF;";
+  &::placeholder{
+    color: #DBDBDB;
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 19.976px;
+    line-height: 25px;
+  }
+`;
+
+const Button = styled.button`
+  width: 300px;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  border-radius: 4.6px;
+  background: #52B6FF;
+ 
+ font-family: 'Lexend Deca';
+ font-style: normal;
+ font-weight: 400;
+ font-size: 20.976px;
+ line-height: 26px;
+ text-align: center;
+ color: #FFFFFF;
+`;
+
+const StyledLink = styled(Link)`
+ width: 210px;
+ height: 17px;
+ font-family: 'Lexend Deca';
+ font-style: normal;
+ font-weight: 400;
+ font-size: 13.976px;
+ line-height: 17px;
+ text-align: center;
+ text-decoration-line: underline;
+ color: #52B6FF;
+`;
---
+++ src/libs/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { ThreeDots } from "react-loader-spinner";
+
+export default function Spinner(){
+    return(
+        <ThreeDots 
+            height="10"
+            color='white'
+            ariaLabel='loading'
+        />
+    )
+}
+
+ 
